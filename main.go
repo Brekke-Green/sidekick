92162faf2c8ef4bfb0616ba5ee0c6eae516472ce
Brekke Green <brekke.andrew.green@gmail.com>
2024-01-04 10:38:37 -0500

    Test sound using beep package

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "os"
+    "time"
+
+    "github.com/gopxl/beep"
+    "github.com/gopxl/beep/speaker"
+    "github.com/gopxl/beep/mp3"
 )
 
 func main() {
-    fmt.Println("Initial commit")
+    f, err := os.Open("619_E_Fifth_St.m4a")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    streamer, format, err := mp3.Decode(f)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer streamer.Close()
+
+    speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+    done := make(chan bool)
+    speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+        done <- true
+    })))
+
+    <-done
 }
